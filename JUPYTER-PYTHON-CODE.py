8b0c84d0fb074ec1869d7bf301f7dfe4d8c2883c
daseanbennett3 <144066858+daseanbennett3@users.noreply.github.com>
2024-05-23 17:45:16 -0700

    Update JUPYTER-PYTHON-CODE.py

--- JUPYTER-PYTHON-CODE.py
+++ JUPYTER-PYTHON-CODE.py
@@ -1,15 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
-#originally named Untitled-Copy1.py
-
+#PYTHON CODE USING JUPYTER NOTEBOOKS
+--
 # In[1]:
 #CLEANING THE DATA (1-8)
-#1. connect to kaggle to download from it
+#1. connect to Kaggle to download from it
 get_ipython().system('pip install kaggle')
 import kaggle
 
 # In[2]:
-#1. Download the orders.csv file from https://www.kaggle.com/datasets/ankitbansal06/retail-orders
+#1. Download the orders.csv file from https://www.kaggle.com/datasets/ankitbansal06/retail-orders to your local system
+# The file will be placed in your "SQL + Python" file
 get_ipython().system('kaggle datasets download ankitbansal06/retail-orders -f orders.csv')
 
 # In[3]:
@@ -64,24 +63,32 @@ df.drop(columns=['cost_price','list_price','discount_percent'],inplace=True)
 # To only display 3 rows:
 df.head(3)
 
+--
+# In[1]:
 #CONNECT TO THE DATA (1-3)
-#1. import SQL Alchemy because its better Pythonicaly than SQL
+#1. Import SQL Alchemy because it's better Pythonicaly than SQL
 import sqlalchemy as sal
-#2. Create the connection. Use the name "DESKTOP-3MQELCQ" which was found in Microsoft SQL Server Management Studio
-#       Use "master" because that is the main database
-#       Use "ODBC+DRIVER+17+FOR+SQL+SERVER" as the driver which was found by searching 'ODBC Data Sources (32-bit)''
+
+# In[2]:
+#1. Create the connection. Use the name "DESKTOP-3MQELCQ" which was found in Microsoft SQL Server Management Studio
+#       Use "master" because that is the main database we will be using
+#       Use "ODBC+DRIVER+17+FOR+SQL+SERVER" as the "driver" which was found by searching 'ODBC Data Sources (32-bit)''
 engine = sal.create_engine('mssql://DESKTOP-3MQELCQ/master?driver=ODBC+DRIVER+17+FOR+SQL+SERVER')
-#3. Connect to the database
+
+# In[3]:
+#1. Connect to the database
 conn=engine.connect()
 
-#LOAD THE DATA INTO THE DATABASE
+--
+# In[1]:
+#LOAD THE DATA INTO THE DATABASE (1-2)
 #1. Use to_sql to load this table named "df_orders" to sql
 #      Set the "index" equal to "False" so the file isnt indexed starting at 0, but rather the order_id
 #      Con (connect) to the connection created in the previous code
 #      "Append" to the table rather than "replace" so you are just adding to it
 df.to_sql('df_orders', con=conn , index=False, if_exists = 'append')
 
-# In[ ]:
+# In[2]:
 #1. Replace the data types of the data (in MySQL Server):
 #1. DROP TABLE df_orders
 #2. CREATE TABLE df_orders
@@ -94,4 +101,6 @@ df.to_sql('df_orders', con=conn , index=False, if_exists = 'append')
 #4. In MySQL Server, run: 
 # SELECT * FROM df_orders (refresh if needed)
 
-
+# IGNORE THESE
+#!/usr/bin/env python
+# coding: utf-8
